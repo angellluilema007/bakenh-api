d9977149c8b465a0e2f519a48b6e5a2208e2e207
angellluilema007 <62965173+angellluilema007@users.noreply.github.com>
2023-09-02 17:36:56 -0500

    evd

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Importa el middleware CORS
+require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
