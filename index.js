7cc11a0f7e88de91c97ee02bc972debd511003c8
angellluilema007 <62965173+angellluilema007@users.noreply.github.com>
2023-09-02 16:49:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000; // Puedes cambiar el puerto si lo deseas
+
+app.use(bodyParser.json());
+
+// Ruta para recibir los datos desde la aplicación Flutter
+app.post('/guardar_datos', (req, res) => {
+  const datos = req.body; // Los datos enviados desde Flutter estarán en req.body
+  
+  // Aquí puedes realizar acciones con los datos, como guardarlos en una base de datos
+  
+  // Envía una respuesta de confirmación
+  res.json({ mensaje: 'Datos recibidos con éxito' });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
