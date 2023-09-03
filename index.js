adcffcd3f3247416bbca56ac6c5c8c6690663234
angellluilema007 <62965173+angellluilema007@users.noreply.github.com>
2023-09-03 17:34:54 -0500

    7sdsdF

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Pool } = require('pg'); // Importa el módulo pg
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
@@ -19,30 +19,48 @@ const pool = new Pool(dbConfig);
 app.use(bodyParser.json());
 app.use(cors());
 
+// POST para guardar datos (tu ruta actual)
 app.post('/guardar_datos', (req, res) => {
   const datos = req.body;
-
-  // Convierte latitud y longitud a números
   const latitud = parseFloat(datos.latitud);
   const longitud = parseFloat(datos.longitud);
-  
-  // Conecta a la base de datos PostgreSQL
+
   pool.connect((err, client, done) => {
     if (err) {
       console.error('Error al conectar a la base de datos:', err);
       res.status(500).json({ error: 'Error al conectar a la base de datos' });
       return;
     }
-    
-    // Realiza la inserción de datos en la base de datos
+
     client.query('INSERT INTO ubicaciones2 (latitud, longitud) VALUES ($1, $2)', [latitud, longitud], (err, result) => {
-      done(); // Libera el cliente de la piscina de conexiones
-      console.log(latitud +"-------"+ longitud)
+      done();
+      console.log(latitud + "-------" + longitud)
       if (err) {
         console.error('Error al ejecutar la consulta:', err);
         res.status(500).json({ error: 'Error al ejecutar la consulta en la base de datos' });
       } else {
         res.json({ mensaje: 'Datos recibidos y guardados con éxito' });
+      }
+    });
+  });
+});
+
+// GET para obtener todos los datos
+app.get('/obtener_datos', (req, res) => {
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      res.status(500).json({ error: 'Error al conectar a la base de datos' });
+      return;
+    }
+
+    client.query('SELECT * FROM ubicaciones2', (err, result) => {
+      done();
+      if (err) {
+        console.error('Error al ejecutar la consulta:', err);
+        res.status(500).json({ error: 'Error al ejecutar la consulta en la base de datos' });
+      } else {
+        res.json(result.rows); // Envía todos los datos como respuesta JSON
       }
     });
   });
